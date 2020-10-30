5adbb8c91deae887ea563a676e50ed2825f27802
Pedro _ <antunes-pedro@hotmail.com>
2020-10-30 22:32:38 +0000

    Alteração do numero minimo de carateres

---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        [StringLength(80, MinimumLength = 2, ErrorMessage ="The name should have at least 2 chars and a maximum of 80 chars")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email")]
+        [EmailAddress(ErrorMessage ="Please enter a valid email")]
+        public string Email { get; set; }
+
+        public bool? WillAttend { get; set; }
+    }
+}
